03b14370d2fc9f3a7ba466d2759c892b942b4178
David Liu <davidliu@Davids-MacBook-Air.local>
2024-01-16 17:26:42 +0800

    fixed resume link

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ function HomePage() {
                     </Heading>
                     <List pl={2}>
                     <ListItem>
-                        <Link href="/files/David Liu Resume May 2023.pdf" download target="_blank">
+                        <Link href="/files/David Liu Resume Jan 2024.pdf" download target="_blank">
                         <Button
                             variant="ghost"
                             colorScheme="teal"
